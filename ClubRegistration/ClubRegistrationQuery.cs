7c4670927f5c8e3bcd4058eba9061f2815fec464
Administrator <Administrator@ASUS>
2025-11-19 23:14:01 +0800

    DataSource Updated

--- ClubRegistration/ClubRegistrationQuery.cs
+++ ClubRegistration/ClubRegistrationQuery.cs
@@ -27,7 +27,7 @@ namespace ClubRegistration
 
         public ClubRegistrationQuery()
         {
-            connectionString = @"Data Source=LAB-A-PC00";
+            connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Administrator\Documents\ClubDB.mdf;Integrated Security=True;Connect Timeout=30";
             sqlConnect = new SqlConnection(connectionString);
             dataTable = new DataTable();
             bindingSource = new BindingSource();
@@ -37,10 +37,11 @@ namespace ClubRegistration
         {
             try
             {
-                string ViewClubMembers = @"Select StudentID,FirstName,MiddleName,LastName,Age,Gender,Program";
+                string ViewClubMembers = @"Select StudentID,FirstName,MiddleName,LastName,Age,Gender,Program FROM ClubMembers";
                 sqlAdapter = new SqlDataAdapter(ViewClubMembers, sqlConnect);
                 dataTable.Clear();
                 sqlAdapter.Fill(dataTable);
+                bindingSource.DataSource = dataTable;
                 return true;
             }
             catch (Exception EX)
@@ -50,6 +51,40 @@ namespace ClubRegistration
 
             }
 
+        }
+        public bool RegisterStudent(int ID, long StudentID, string FirstName,
+                                string MiddleName, string LastName,
+                                int Age, string Gender, string Program)
+        {
+            try
+            {
+                sqlCommand = new SqlCommand(
+                    "INSERT INTO ClubMembers (ID, StudentId, FirstName, MiddleName, LastName, Age, Gender, Program) " +
+                    "VALUES(@ID, @StudentID, @FirstName, @MiddleName, @LastName, @Age, @Gender, @Program)",
+                    sqlConnect);
+
+                sqlCommand.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
+                sqlCommand.Parameters.Add("@StudentID", SqlDbType.BigInt).Value = StudentID;
+                sqlCommand.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = FirstName;
+                sqlCommand.Parameters.Add("@MiddleName", SqlDbType.VarChar).Value = MiddleName;
+                sqlCommand.Parameters.Add("@LastName", SqlDbType.VarChar).Value = LastName;
+                sqlCommand.Parameters.Add("@Age", SqlDbType.Int).Value = Age;
+                sqlCommand.Parameters.Add("@Gender", SqlDbType.VarChar).Value = Gender;
+                sqlCommand.Parameters.Add("@Program", SqlDbType.VarChar).Value = Program;
+
+                sqlConnect.Open();
+                sqlCommand.ExecuteNonQuery();
+                sqlConnect.Close();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if (sqlConnect.State == ConnectionState.Open)
+                    sqlConnect.Close();
+                MessageBox.Show("Error inserting student: " + ex.Message);
+                return false;
+            }
         }
     }
 }
