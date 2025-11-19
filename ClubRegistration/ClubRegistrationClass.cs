bea8c78fba63ed54afa0955c0e50e60e390dc783
pauljohnLLL <pauljohnluna7@gmail.com>
2025-11-19 19:44:44 +0800

    ClubRegistrationQueryCreated

---
+++ ClubRegistration/ClubRegistrationClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubRegistration
+{
+    internal class ClubRegistrationClass
+    {
+        public static int SetStudID = 0;
+        public static int SetContactNo = 0;
+        public static string SetProgram = "";
+        public static string SetGender = "";
+        public static string SetBirthday = "";
+        public static string SetFullName = "";
+        public static int SetAge = 0;
+    }
+}
---
+++ ClubRegistration/ClubRegistrationQuery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClubRegistration
+{
+    internal class ClubRegistrationQuery
+    {
+        private SqlConnection sqlConnect;
+        private SqlCommand sqlCommand;
+        private SqlDataAdapter sqlAdapter;
+        private SqlDataReader sqlReader;
+        public DataTable dataTable;
+        public BindingSource bindingSource;
+        private string connectionString;
+        public string _FirstName, _MiddleName, _LastName, _Gender, _Program;
+        int _Age;
+    }
+}
