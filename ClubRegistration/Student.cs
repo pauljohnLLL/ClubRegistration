fa1d89f3106cbfb70efd66f75154150f9253f0b0
Administrator <Administrator@ASUS>
2025-11-20 02:11:48 +0800

    Student class created

---
+++ ClubRegistration/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubRegistration
+{
+    internal class Student
+    {       
+            public int ID { get; set; }
+            public long StudentId { get; set; }
+            public string FirstName { get; set; }
+            public string MiddleName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+            public string Gender { get; set; }
+            public string Program { get; set; }
+        }
+
+    }
+
