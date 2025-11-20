f5797787d261e249be4ba42cabff82a6e4afe78f
Administrator <Administrator@ASUS>
2025-11-20 09:45:52 +0800

    Updated Connection string

--- ClubRegistration/FrmUpdateMember.cs
+++ ClubRegistration/FrmUpdateMember.cs
@@ -14,59 +14,98 @@ namespace ClubRegistration
 {
     public partial class FrmUpdateMember : Form
     {
-        private long _studentId;
-        private ClubRegistrationQuery clubRegistrationQuery;
+        private SqlConnection sqlConnect;
+        private SqlCommand sqlCommand;
+        private SqlDataReader sqlReader;
+        private string connectionString =
+    @"Data Source=(LocalDB)\MSSQLLocalDB;
+      AttachDbFilename=C:\Users\Administrator\Documents\ClubDB.mdf;
+      Integrated Security=True;
+      Connect Timeout=30";
 
-        public FrmUpdateMember(long studentId)
+
+        private ClubRegistrationQuery clubQuery;
+
+        public FrmUpdateMember()
         {
             InitializeComponent();
-
-            _studentId = studentId;
-            clubRegistrationQuery = new ClubRegistrationQuery();
+            clubQuery = new ClubRegistrationQuery();
         }
 
         private void FrmUpdateMember_Load(object sender, EventArgs e)
         {
-            LoadStudentData();
-        }
+            sqlConnect = new SqlConnection(connectionString);
+            LoadStudentIds();
 
-        private void LoadStudentData()
-        {
-            var student = clubRegistrationQuery.GetStudentById(_studentId);
-
-            if (student != null)
+            cbGender.Items.AddRange(new string[] { "Male", "Female" });
+            cbProgram.Items.AddRange(new string[]
             {
-                txtStudentID.Text = student.StudentId.ToString();
-                txtLastName.Text = student.LastName;
-                txtFirstName.Text = student.FirstName;
-                txtMiddleName.Text = student.MiddleName;
-                txtAge.Text = student.Age.ToString();
-                cbGender.Text = student.Gender;
-                cbProgram.Text = student.Program;
-            }
+                "BS Information Technology", "BS Computer Science",
+                "BS Information Systems", "BS in Accountancy",
+                "BS in Hospitality Management", "BS in Tourism Management"
+            });
+
+            txtStudentID.SelectedIndexChanged += txtStudentID_SelectedIndexChanged;
         }
 
+        private void LoadStudentIds()
+        {
+            txtStudentID.Items.Clear();
+            sqlConnect.Open();
+            sqlCommand = new SqlCommand("SELECT StudentId FROM ClubMembers", sqlConnect);
+            sqlReader = sqlCommand.ExecuteReader();
+
+            while (sqlReader.Read())
+            {
+                txtStudentID.Items.Add(sqlReader["StudentId"].ToString());
+            }
+
+            sqlReader.Close();
+            sqlConnect.Close();
+        }
+
+        private void txtStudentID_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sqlConnect.Open();
+            sqlCommand = new SqlCommand(
+                "SELECT * FROM ClubMembers WHERE StudentId=@StudentId", sqlConnect);
+            sqlCommand.Parameters.AddWithValue("@StudentId", txtStudentID.Text);
+
+            sqlReader = sqlCommand.ExecuteReader();
+
+            if (sqlReader.Read())
+            {
+                txtFirstName.Text = sqlReader["FirstName"].ToString();
+                txtMiddleName.Text = sqlReader["MiddleName"].ToString();
+                txtLastName.Text = sqlReader["LastName"].ToString();
+                txtAge.Text = sqlReader["Age"].ToString();
+                cbGender.Text = sqlReader["Gender"].ToString();
+                cbProgram.Text = sqlReader["Program"].ToString();
+            }
+
+            sqlReader.Close();
+            sqlConnect.Close();
+        }
         private void button3_Click(object sender, EventArgs e)
         {
-            bool ok = clubRegistrationQuery.UpdateStudent(
-                _studentId,                   
-                long.Parse(txtStudentID.Text), 
-                txtFirstName.Text,
-                txtMiddleName.Text,
-                txtLastName.Text,
-                int.Parse(txtAge.Text),
-                cbGender.Text,
-                cbProgram.Text
-            );
+            bool ok = clubQuery.UpdateStudent(
+               long.Parse(txtStudentID.Text),
+               txtFirstName.Text,
+               txtMiddleName.Text,
+               txtLastName.Text,
+               int.Parse(txtAge.Text),
+               cbGender.Text,
+               cbProgram.Text
+           );
 
             if (ok)
             {
                 MessageBox.Show("Member updated successfully!");
                 this.Close();
             }
-
         }
     }
 }
+
 
 
